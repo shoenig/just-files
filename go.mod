60b900ff31a9caa7737a32b0364dab742a732abd
Seth Hoenig <shoenig@hashicorp.com>
2021-07-08 10:31:33 -0500

    fix some things

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
-module gophers.dev/cmds/static-files
+module gophers.dev/cmds/connect-statics
 
 go 1.16
+
+require (
+	github.com/hashicorp/consul v1.10.0
+	github.com/hashicorp/consul/api v1.9.0
+)
--- main.go
+++ main.go
@@ -2,34 +2,43 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"time"
+
+	"github.com/hashicorp/consul/api"
+	"github.com/hashicorp/consul/connect"
 )
 
-// $ static-files [<url-path>:<file-path>, ...]
+// $ connect-static-files [<url-path>:<file-path>, ...]
 
 func main() {
-	m, err := args(os.Args[1:])
+	m, err := paths(os.Args[1:])
 	check(err)
 
-	mux := router(m)
+	service := getStringOr("SERVICE", "connect-static")
+	bind := getStringOr("BIND", "0.0.0.0")
+	port := mustGetInt("PORT")
 
-	_ = mux
+	cs, err := connect.NewService(service, consul())
+	check(err)
+
 	s := http.Server{
-		Addr:              "0.0.0.0:8000",
-		Handler:           mux,
-		TLSConfig:         nil,
+		Addr:              fmt.Sprintf("%s:%d", bind, port),
+		Handler:           router(m),
+		TLSConfig:         cs.ServerTLSConfig(),
 		ReadTimeout:       5 * time.Second,
 		ReadHeaderTimeout: 1 * time.Second,
 		WriteTimeout:      5 * time.Second,
 	}
 
-	check(s.ListenAndServe())
+	check(s.ListenAndServeTLS("", ""))
 }
 
-func args(args []string) (map[string]string, error) {
+func paths(args []string) (map[string]string, error) {
 	if len(args) == 0 {
 		return nil, fmt.Errorf("must provide at least one path pair")
 	}
@@ -40,7 +49,7 @@ func args(args []string) (map[string]string, error) {
 		if len(tokens) != 2 {
 			return nil, fmt.Errorf("arg format must be <url-path>:<file-path>")
 		}
-		m[tokens[0]] = m[tokens[1]]
+		m[tokens[0]] = tokens[1]
 	}
 	return m, nil
 }
@@ -48,7 +57,8 @@ func args(args []string) (map[string]string, error) {
 func router(m map[string]string) http.Handler {
 	mux := http.NewServeMux()
 	for urlPath, filePath := range m {
-		mux.Handle(urlPath, http.StripPrefix(filePath, http.FileServer(http.Dir(filePath))))
+		fmt.Printf("mapping %s -> %s\n", urlPath, filePath)
+		mux.Handle(urlPath, http.StripPrefix(urlPath, http.FileServer(http.Dir(filePath))))
 	}
 	return mux
 }
@@ -58,4 +68,52 @@ func check(err error) {
 		fmt.Println("fatal:", err)
 		os.Exit(1)
 	}
+}
+
+// todo: refactor these helpers
+
+func consul() *api.Client {
+	logEnvironment("CONSUL_HTTP_ADDR")
+	logEnvironment("CONSUL_NAMESPACE")
+	logEnvironment("CONSUL_CACERT")
+	logEnvironment("CONSUL_CLIENT_CERT")
+	logEnvironment("CONSUL_CLIENT_KEY")
+	logEnvironment("CONSUL_HTTP_SSL")
+	logEnvironment("CONSUL_HTTP_SSL_VERIFY")
+	logEnvironment("CONSUL_TLS_SERVER_NAME")
+	logEnvironment("CONSUL_GRPC_ADDR")
+	logEnvironment("CONSUL_HTTP_TOKEN_FILE")
+	consulConfig := api.DefaultConfig()
+	consulClient, err := api.NewClient(consulConfig)
+	if err != nil {
+		log.Fatal("failed to make consul client:", err)
+	}
+	return consulClient
+}
+
+func logEnvironment(name string) {
+	value := os.Getenv(name)
+	if value == "" {
+		value = "<unset>"
+	}
+	log.Printf("environment %s = %s", name, value)
+}
+
+func mustGetInt(name string) int {
+	if s := os.Getenv(name); s != "" {
+		p, err := strconv.Atoi(s)
+		if err != nil {
+			log.Fatal(name + " must be a number")
+		}
+		return p
+	}
+	log.Fatal(name + " must be set")
+	return -1
+}
+
+func getStringOr(name, alt string) string {
+	if s := os.Getenv(name); s != "" {
+		return s
+	}
+	return alt
 }
